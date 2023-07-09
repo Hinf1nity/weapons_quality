858e117685f3219a8a8163f5ddb16f3527671de6
Hinf1nity <dacalvimontes@gmail.com>
2023-07-08 21:36:34 -0400

    Update get-version.js

--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -1,2 +1,2 @@
-let fs = rquire('fs')
-console.log(JSON.parse(fs.readFileSync('/module.json', 'utf8')).version)
+let fs = require('fs')
+console.log(JSON.parse(fs.readFileSync('./module.json', 'utf8')).version)
